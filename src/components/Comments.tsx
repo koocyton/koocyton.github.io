ab67a3432c3bb9a903aa54aeb3b3fe662319d28b
Henry <henry@5163.xyz>
2026-02-20 21:54:31 +0800

    giscus

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,17 +1,21 @@
 "use client";
 
 import Giscus from "@giscus/react";
+import { usePathname } from "next/navigation";
 
 export default function Comments() {
+  const pathname = usePathname();
+
   return (
     <div className="mt-12 pt-6 border-t border-[var(--color-border)]">
       <Giscus
+        key={pathname}
         repo="koocyton/koocyton.github.io"
         repoId="MDEwOlJlcG9zaXRvcnkxNDY5NTUzMzQ="
         category="General"
         categoryId="DIC_kwDOCMJcRs4C225y"
         mapping="pathname"
-        strict="0"
+        strict="1"
         reactionsEnabled="1"
         emitMetadata="0"
         inputPosition="top"
